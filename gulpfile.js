f651ecd6a25765a4b25b7d8debc30f82c4d873af
varyzheng <varyzheng@outlook.com>
2017-11-10 18:10:33 +0800

    gulp + left-menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,34 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
- 
-gulp.task('webserver', function() {
-  gulp.src('src')
+var fileinclude = require('gulp-file-include');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('webserver', ['fileinclude', 'minCss', 'syncLib'], function() {
+
+  gulp.src('./')
     .pipe(webserver({
       livereload: true,
       directoryListing: true,
       open: true
-    }));
+  }));
+
 });
+
+gulp.task('fileinclude', function() {
+  gulp.src(['./src/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+  })).pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minCss', function() {
+  gulp.src(['./src/css/**/*.css']).pipe(cleanCSS()).pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('syncLib', function(){
+  gulp.src(['./src/lib/**/*']).pipe(gulp.dest('./dist/lib/'));
+});
+
+gulp.watch('./src/**/*.html', ['fileinclude']);
+gulp.watch('./src/css/**/*.css', ['minCss']);
